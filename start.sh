480c1cde355e9204480aacf2f5a63a04dcdeebac
Corentin Dupont <corentin.dupont@gmail.com>
2019-09-19 16:55:02 +0200

    NO_LORA env var

--- start.sh
+++ start.sh
@@ -4,4 +4,14 @@ chmod +x /app/data_acq/lora/lora_gateway
 python /app/setup.py
 
 python /app/api.py & 
-python /app/startLora.py
+
+noLora=$NO_LORA
+if [ "$noLora" = true ]; then
+      echo "Lora not started"
+else
+      python /app/startLora.py &
+fi
+
+#Waiting for Ctrl-C
+while :; do sleep 1; done
+
