6dbb08007369313b1ed6b5d80ae043e9aa4a1299
Johann Forster <25847959+j-forster@users.noreply.github.com>
2021-10-21 10:52:25 +0200

    Cleanup

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/Waziup/wazigate-system
 
-go 1.15
+go 1.16
 
 require (
 	github.com/julienschmidt/httprouter v1.3.0
--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -59,7 +59,11 @@ const common = {
 			},
 			{
 				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-				loader: "url-loader?limit=10000&mimetype=application/font-woff"
+				loader: "url-loader",
+				options: {
+					limit:10000,
+					mimetype: "application/font-woff"
+				}
 			},
 			{
 				test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
