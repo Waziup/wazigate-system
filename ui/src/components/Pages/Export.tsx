e7aa6fe3c6475a629380e99f00e83cebd52b901c
Felix Markwordt <24inchstreetbikesoldier@gmail.com>
2023-03-21 10:19:41 +0100

    changed path of api call for export

--- ui/src/components/Pages/Export.tsx
+++ ui/src/components/Pages/Export.tsx
@@ -111,7 +111,7 @@ class PageExport extends React.Component<{},State> {
                           })
                         }}>
                   </input>
-                  <MDBBtn disabled={false} href={"../../../exportbins?from="+this.convTime(this.state.fromTime)+"-0000"+"&to="+this.convTime(this.state.toTime)+"-0000"+"&duration="+this.state.duration.toString()+"m"}>
+                  <MDBBtn disabled={false} href={"../../../exportbins?from="+this.convTimeWithSec(this.state.fromTime)+"-0000"+"&to="+this.convTimeWithSec(this.state.toTime)+"-0000"+"&duration="+this.state.duration.toString()+"m"}>
                         <MDBIcon
                           icon="account_tree"
                           className="ml-2"
