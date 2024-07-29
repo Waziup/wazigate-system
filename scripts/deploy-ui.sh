58e0e8fab568c3308b13f8729bc0eeed2b994435
OliverMengich <kemeioliver2000@gmail.com>
2024-07-29 14:14:53 +0300

    refactor: removes UI contents

--- scripts/deploy-ui.sh
+++ scripts/deploy-ui.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
-WAZIGATE_TAG="${WAZIGATE_TAG:-latest}"
-WAZIGATE_ADDR="${WAZIGATE_ADDR:-raspberrypi.local}"
-WAZIGATE_PASS="${WAZIGATE_PASS:-raspberry}"
-WAZIGATE_USER="${WAZIGATE_USER:-pi}"
+# WAZIGATE_TAG="${WAZIGATE_TAG:-latest}"
+# WAZIGATE_ADDR="${WAZIGATE_ADDR:-raspberrypi.local}"
+# WAZIGATE_PASS="${WAZIGATE_PASS:-raspberry}"
+# WAZIGATE_USER="${WAZIGATE_USER:-pi}"
 
-echo "[1 / 3] Test ping ..."
-ping -c 1 $WAZIGATE_ADDR
+# echo "[1 / 3] Test ping ..."
+# ping -c 1 $WAZIGATE_ADDR
 
-echo "[2 / 3] Copy files to remote ..."
-sshpass -p $WAZIGATE_PASS rsync --relative -a \
-    ./ui/./node_modules/react/umd \
-    ./ui/./node_modules/react-dom/umd \
-    ./ui/./index.html \
-    ./ui/./dev.html \
-    ./ui/./favicon.ico  \
-    ./ui/./icons \
-    ./ui/./dist \
-    $WAZIGATE_USER@$WAZIGATE_ADDR:/home/pi/wazigate-system-ui/
+# echo "[2 / 3] Copy files to remote ..."
+# sshpass -p $WAZIGATE_PASS rsync --relative -a \
+#     ./ui/./node_modules/react/umd \
+#     ./ui/./node_modules/react-dom/umd \
+#     ./ui/./index.html \
+#     ./ui/./dev.html \
+#     ./ui/./favicon.ico  \
+#     ./ui/./icons \
+#     ./ui/./dist \
+#     $WAZIGATE_USER@$WAZIGATE_ADDR:/home/pi/wazigate-system-ui/
 
-echo "[3 / 3] Copy files to container ..."
-sshpass -p $WAZIGATE_PASS ssh -T $WAZIGATE_USER@$WAZIGATE_ADDR <<EOF
-docker cp wazigate-system-ui/. waziup.wazigate-system:/app/ui/
-EOF
+# echo "[3 / 3] Copy files to container ..."
+# sshpass -p $WAZIGATE_PASS ssh -T $WAZIGATE_USER@$WAZIGATE_ADDR <<EOF
+# docker cp wazigate-system-ui/. waziup.wazigate-system:/app/ui/
+# EOF
 
-echo "[     ] OK"
+# echo "[     ] OK"
