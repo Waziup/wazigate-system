287ba6ccfbedf7f42eeebb8c0c5a3e64fb00c287
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2026-08-07 12:46:21 +0300

    refactor: remove VPN helper script installation

--- pkg/nm/nm.go
+++ pkg/nm/nm.go
@@ -501,22 +501,8 @@ func ConnectVPN( conn gonetworkmanager.Connection) error {
 		return fmt.Errorf("error getting VPN state %v", err)
 	}
 	log.Printf("VPN State: %v", state)
-	if ok := fileExists("/etc/NetworkManager/dispatcher.d/vpn-helper"); !ok {
-		err = installExecutable(
-			"/etc/NetworkManager/dispatcher.d/vpn-helper",
-		)
-		if err != nil {
-			log.Printf("Failed to install VPN helper: %v", err)
-		} else {
-			log.Println("VPN helper installed successfully")
-		}
-	}
 	log.Println("VPN connected successfully!")
 	return nil
-}
-func fileExists(path string) bool {
-	_, err := os.Stat(path)
-	return err == nil
 }
 func ImportVPN(configFile string) (gonetworkmanager.Connection, error) {
 	log.Println("Importing VPN profile...")
@@ -549,138 +535,9 @@ func ImportVPN(configFile string) (gonetworkmanager.Connection, error) {
             return nil, err
         }
 	}
-	err = installExecutable(
-		"/etc/NetworkManager/dispatcher.d/vpn-helper",
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("VPN helper installed successfully")
+	
+	log.Println("VPN setup successfully")
 	return conn, nil
-}
-
-func installExecutable(dst string) error {
-	helperScript := `#!/bin/bash
-IFACE="$1"
-STATUS="$2"
-
-LOG_FILE="/tmp/openvpn-dispatcher.log"
-VPN_PREFIX="gateway-"
-
-touch "$LOG_FILE"
-
-log() {
-	echo "$(date '+%F %T') $1" >> "$LOG_FILE"
-}
-
-if [ "$STATUS" != "up" ] || [ "$IFACE" = "lo" ]; then
-	log "Connection is not up or Interface $IFACE is loopback, exiting."
-	exit 0
-fi
-
-case "$IFACE" in
-	docker*|br-*|tun*|veth*|vbox*)
-		log "Interface $IFACE is a virtual interface, skipping VPN connection."
-		exit 0
-		;;
-	*)
-		log "Interface $IFACE is up. Procceding with vpn connection..."
-		;;
-esac
-
-log "Interface $IFACE is up, waiting for NetworkManager..."
-
-
-for i in {1..30}; do
-	NM_STATE=$(nmcli general status | awk 'NR==2 {print $1}')
-
-	if [ "$NM_STATE" = "connected" ]; then
-		log "NetworkManager is connected"
-		break
-	fi
-
-	sleep 1
-done
-
-if [ "$NM_STATE" != "connected" ]; then
-	log "NetworkManager not ready, exiting"
-	exit 0
-fi
-
-log "Verifying internet connectivity..."
-INTERNET_READY=0
-for i in {1..35}; do
-	if ping -c 1 -W 1 8.8.8.8 >/dev/null 2>&1; then
-		log "Connected to internet with interface $IFACE."
-		INTERNET_READY=1
-		break
-	fi
-	log "Not connected yet, waiting... (Attempt $i/35)"
-	sleep 1
-done
-
-if [ "$INTERNET_READY" -ne 1 ]; then
-	log "ERROR: NetworkManager is up but public internet is unreachable. Exiting."
-	exit 0
-fi
-
-log "Fetching VPN name from API..."
-
-while true; do
-	VPN_RAW=$(curl -s --max-time 5 http://localhost/device/id | xargs)
-
-	if [ -n "$VPN_RAW" ]; then
-		log "Received VPN name: $VPN_RAW"
-		break
-	fi
-
-	log "API unavailable or returned empty response, retrying in 1 second..."
-	sleep 1
-done
-log "Found gateway id $VPN_RAW"
-
-VPN_NAME="${VPN_PREFIX}${VPN_RAW}"
-
-log "Resolved VPN name: $VPN_NAME"
-
-
-if ! nmcli connection show | awk '{print $1}' | grep -qx "$VPN_NAME"; then
-	log "VPN connection '$VPN_NAME' does not exist"
-	exit 0
-fi
-
-
-if nmcli connection show --active | awk '{print $1}' | grep -qx "$VPN_NAME"; then
-	log "VPN already active, skipping"
-	exit 0
-fi
-
-
-log "Bringing up VPN: $VPN_NAME"
-
-success=0
-
-for i in {1..5}; do
-	if nmcli connection up "$VPN_NAME" >> "$LOG_FILE" 2>&1; then
-		log "VPN connection succeeded on attempt $i"
-		success=1
-		break
-	fi
-	sleep 3
-done
-
-if [ "$success" -ne 1 ]; then
-	log "VPN connection failed after 5 attempts"
-fi
-log "VPN start command executed"
-	`
-	if err := os.WriteFile(dst, []byte(helperScript), 0755); err != nil {
-		return err
-	}
-	if err := os.Chown(dst, 0, 0); err != nil {
-		return err
-	}
-	return nil
 }
 func VpnProfileExists(clientID string) (gonetworkmanager.Connection, bool, error) {
 	connections,err :=settings.ListConnections()
